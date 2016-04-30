9959c22090cdc6d59896022627493facfcee055f
Esther Jun Kim <esther.jun.kim@gmail.com>
2016-04-29 18:03:52 -0700

    Add player

--- js/app.js
+++ js/app.js
@@ -1,36 +1,94 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.col = 2;
+  this.row = 5;
+  this.x = this.col * 101;
+  this.y = this.row * 83;
+};
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+
+  if(this.col < 0) {
+    this.col = 0;
+  }
+
+  if(this.col > 4) {
+    this.col = 4;
+  }
+
+  if(this.row < 0) {
+    this.row = 0;
+  }
+
+  if(this.row > 5) {
+    this.row = 5;
+  }
+
+  this.x = this.col * 101;
+  this.y = this.row * 83;
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Handle user input for controlling the player
+Player.prototype.handleInput = function(key) {
+  switch(key) {
+    case 'left':
+      this.col--;
+      break;
+    case 'right':
+      this.col++;
+      break;
+    case 'up':
+      this.row--;
+      break;
+    case 'down':
+      this.row++;
+      break;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
