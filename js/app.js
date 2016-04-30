d1cf205ce54a71bacb612d915e15461d7f94375f
Esther Jun Kim <esther.jun.kim@gmail.com>
2016-04-29 18:27:18 -0700

    Remove 'dt' argument for Player.prototype.update()... not needed

--- js/app.js
+++ js/app.js
@@ -34,12 +34,7 @@ var Player = function() {
 };
 
 // Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-
+Player.prototype.update = function() {
   if(this.col < 0) {
     this.col = 0;
   }
